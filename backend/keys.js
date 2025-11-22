15558cf3b08a595da02f9e811aec1ffecc7d9f82
Dua Mansoor <142514734+DuaMansoor@users.noreply.github.com>
2025-11-22 23:53:20 +0500

    keys.js

--- backend/keys.js
+++ backend/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoUrl: "mongodb+srv://duamansoor381_db_user:duamansoor@cluster0.xtclqn0.mongodb.net/?appName=Cluster0",
+  mongoUrl: "mongodb+srv://{user_name}_db_user:{password}r@cluster0.xtclqn0.mongodb.net/?appName=Cluster0",
   Jwt_secret: "faslkfocvneofu"
 };
